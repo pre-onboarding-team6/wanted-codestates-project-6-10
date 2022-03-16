2e0d111129632e67b6d9a073bee03acdb958c029
heyJ <90097736+heyjheyj@users.noreply.github.com>
2022-03-16 19:23:20 +0900

    Revert "Feat: Dropdown component 추가"

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import './App.css';
 import { Global } from '@emotion/react';
 import { reset } from './styles/reset';
 
-import SearchForm from './components/SearchForm';
-
 function App() {
   return (
-    <>
+    <div>
       <Global styles={reset} />
-      <SearchForm />
-    </>
+    </div>
   );
 }
 
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import diseaseReducer from './diseaseReducer';
-
-export const store = configureStore({
-  reducer: {
-    data: diseaseReducer,
-  },
-});
